26919c62932c681b8adec85b404bd915f57ae8e3
SR Tamim <saifur.rahman.tamim1@gmail.com>
2023-01-22 16:23:06 +0600

    ui improvement

--- src/pages/Submissions.jsx
+++ src/pages/Submissions.jsx
@@ -15,17 +15,20 @@ const Submissions = () => {
     return (<section className="max-w-xl mx-auto">
         {!submissions.length && <div className="animate-pulse text-center my-20">Loading...</div>}
 
-        {submissions.map(item => <Link to={`/submissions/${item._id}`} className="card bg-base-100 shadow-xl">
-            <div className="card-body">
-                <h2 className="card-title capitalize">{item.name}</h2>
-                <div>
-                    {item.sectors.map(sector => <div className="badge badge-primary badge-outline">{sector.name}</div>)}
+        <div className="grid gap-6">
+            <h3 className="mt-10 text-xl font-semibold text-primary">My Submissions</h3>
+            {submissions.map(item => <Link to={`/submissions/${item._id}`} className="card bg-base-100 shadow-lg">
+                <div className="card-body">
+                    <h2 className="card-title capitalize">{item.name}</h2>
+                    <div className="flex flex-wrap gap-2">
+                        {item.sectors.map(sector => <div className="badge badge-primary badge-outline">{sector.name}</div>)}
+                    </div>
+                    <div className="card-actions justify-end">
+                        <div className="text-sm text-gray-500">Submitted at {new Date(item.timestamp).toLocaleString()}</div>
+                    </div>
                 </div>
-                <div className="card-actions justify-end">
-                    <div className="text-sm text-gray-500">Submitted at {new Date(item.timestamp).toLocaleString()}</div>
-                </div>
-            </div>
-        </Link>)}
+            </Link>)}
+        </div>
 
         {!localStorage.getItem('submissions') ? <div className="text-gray-500 my-8">
             You haven't submitted any info yet. <Link to="/" className="underline text-primary">Submit now</Link>
