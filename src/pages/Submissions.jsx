5095f835f8d0c8f4f34eaefd3003066195e063dc
SR Tamim <saifur.rahman.tamim1@gmail.com>
2023-01-22 16:27:24 +0600

    bug fix

--- src/pages/Submissions.jsx
+++ src/pages/Submissions.jsx
@@ -13,28 +13,28 @@ const Submissions = () => {
     }, [])
 
     return (<section className="max-w-xl mx-auto">
-        {!submissions.length && <div className="animate-pulse text-center my-20">Loading...</div>}
-
-        <div className="grid gap-6">
-            <h3 className="mt-10 text-xl font-semibold text-primary">My Submissions</h3>
-            {submissions.map(item => <Link to={`/submissions/${item._id}`} className="card bg-base-100 shadow-lg">
-                <div className="card-body">
-                    <h2 className="card-title capitalize">{item.name}</h2>
-                    <div className="flex flex-wrap gap-2">
-                        {item.sectors.map(sector => <div className="badge badge-primary badge-outline">{sector.name}</div>)}
-                    </div>
-                    <div className="card-actions justify-end">
-                        <div className="text-sm text-gray-500">Submitted at {new Date(item.timestamp).toLocaleString()}</div>
-                    </div>
-                </div>
-            </Link>)}
-        </div>
-
-        {!localStorage.getItem('submissions') ? <div className="text-gray-500 my-8">
+        {localStorage.getItem('submissions') ? <div className="text-gray-500 my-8">
             You haven't submitted any info yet. <Link to="/" className="underline text-primary">Submit now</Link>
-        </div>
-            : <div className="text-center text-primary mt-16 capitalize"
-            ><Link to="/">Submit again</Link></div>}
+        </div> : <>
+            {!submissions.length && <div className="animate-pulse text-center my-20">Loading...</div>}
+
+            <div className="grid gap-6">
+                <h3 className="mt-10 text-xl font-semibold text-primary">My Submissions</h3>
+                {submissions.map(item => <Link to={`/submissions/${item._id}`} className="card bg-base-100 shadow-lg">
+                    <div className="card-body">
+                        <h2 className="card-title capitalize">{item.name}</h2>
+                        <div className="flex flex-wrap gap-2">
+                            {item.sectors.map(sector => <div className="badge badge-primary badge-outline">{sector.name}</div>)}
+                        </div>
+                        <div className="card-actions justify-end">
+                            <div className="text-sm text-gray-500">Submitted at {new Date(item.timestamp).toLocaleString()}</div>
+                        </div>
+                    </div>
+                </Link>)}
+            </div>
+            <div className="text-center text-primary mt-16 capitalize"
+            ><Link to="/">Submit again</Link></div>
+        </>}
     </section>);
 };
 
