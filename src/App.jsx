c6e04984c92da02d00b16a31ad0b3b705bdfc741
SR Tamim <saifur.rahman.tamim1@gmail.com>
2023-01-22 11:44:34 +0600

    axios installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { useState } from "react"
-import AllSectors from "./components/AllSectors"
+
+import Sectors_SelectField from "./components/Sectors_SelectField"
 
 function App() {
-  const [showAllSectors, setShowAllSectors] = useState(false)
-  const [selectedSectors, setSelectedSectors] = useState([])
-  const selectAction = (selected, sector) => {
-    if (selected) {
-      setSelectedSectors(prev => [...prev, sector])
-    } else {
-      setSelectedSectors(prev => prev.filter(item => item.id !== sector.id))
-    }
-  }
-
   return (
     <main className="px-2">
       <h1 className='text-4xl text-center font-bold'>Sector <span className="text-primary">Survey</span></h1>
@@ -30,41 +20,7 @@ function App() {
             <label className="label">
               <span className="label-text">Sectors:</span>
             </label>
-            <div tabIndex={0} onClick={() => setShowAllSectors(prev => !prev)}
-              className="group relative w-full min-h-[3rem] flex items-center gap-2 flex-wrap border border-primary rounded-lg px-4 py-2 focus:outline outline-2 outline-primary outline-offset-2">
-              {!selectedSectors.length ? <span className="text-gray-400">Select</span>
-                : selectedSectors.map(sector => <div key={sector.id}
-                  className="badge badge-sm badge-primary">
-                  {sector.name}
-                </div>)}
-
-              {/* background div to detect outside click */}
-              {showAllSectors && <div className="fixed left-0 top-0 w-full h-full z-[9]"></div>}
-
-              <div onClick={(e) => e.stopPropagation()}
-                className={`absolute top-[110%] left-0 w-full h-44 bg-base-100 overflow-y-auto border border-primary rounded px-4 shadow-lg z-[10] transition origin-top ${showAllSectors ? 'scale-y-100' : 'scale-y-0'}`}>
-                {AllSectors.map(sector => <div className="flex flex-col my-4 text-sm" key={sector.id}>
-
-                  <span className="text-gray-500">{sector.categoryName}</span>
-                  {sector.sectors.map(subSector => <label className="flex gap-2 items-center pl-3 py-1 cursor-pointer" key={subSector.id}>
-                    <input type="checkbox" className="checkbox checkbox-xs checkbox-primary"
-                      defaultChecked={selectedSectors.find(item => item.id === subSector.id)}
-                      onChange={({ target }) => selectAction(target.checked, subSector)} />
-                    {subSector.name}</label>
-                  )}
-
-                  {sector?.subCategories?.map(subCategory => <div key={subCategory.id} className="flex flex-col pl-3 my-2">
-                    <span className="text-gray-500">{subCategory.categoryName}</span>
-                    {subCategory.sectors.map(subSubSector => <label key={subSubSector.id} className="flex gap-2 items-center pl-4 py-1 cursor-pointer">
-                      <input type="checkbox" className="checkbox checkbox-xs checkbox-primary"
-                        defaultChecked={selectedSectors.find(item => item.id === subSubSector.id)}
-                        onChange={({ target }) => selectAction(target.checked, subSubSector)} />
-                      {subSubSector.name}</label>
-                    )}
-                  </div>)}
-                </div>)}
-              </div>
-            </div>
+            <Sectors_SelectField />
           </div>
 
           <div className="form-control w-full max-w-sm mx-auto">
@@ -73,7 +29,10 @@ function App() {
               <span className="label-text">Agree to terms</span>
             </label>
           </div>
-          <button className="btn btn-primary w-full max-w-sm mx-auto">Save</button>
+          <button className="btn btn-primary w-full max-w-sm mx-auto"
+            onClick={() => {
+            }}
+          >Save</button>
         </div>
       </section>
     </main>
