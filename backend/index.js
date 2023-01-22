0646f39eb69553c1496d93c7ad0676313ea61a35
SR Tamim <saifur.rahman.tamim1@gmail.com>
2023-01-22 11:44:09 +0600

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+const app = express();
+const port = 1212;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bdnuvui.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.listen(port, () => console.log(`http:localhost:${port}`));
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('sector_survey');
+        const sectorsCollection = database.collection('sectors');
+
+        app.get('/sectors', async (req, res) => {
+            const cursor = sectorsCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+        });
+        app.post('/sectors', async (req, res) => {
+            const newSector = req.body;
+            const result = await sectorsCollection.insertOne(newSector);
+            res.json(result);
+        })
+    } finally {
+        // await client.close() 
+    }
+}
+
+run().catch(console.dir);
